7ab36ee61baad83a8967dd94a8e2126513fadfd9
Tomasz Siłkowski <tmsilkowski@gmail.com>
2021-06-18 19:59:24 +0200

    Working in both examples

--- EdgeMatch_2.py
+++ EdgeMatch_2.py
@@ -2,6 +2,11 @@ from sage.all import matrix, vector, RR, QQ, show, InteractiveLPProblem, MixedIn
 import sys
 import bisect
 
+N = 6 # NxN grid
+corners = []
+sides = []
+middles = []
+
 # pokazuje intuicyjną reprezentację danego kafelka; wymaga listy stringów z co najmniej 3 elementami
 def showTile(lines, tile):
     lines[-3] += "⌜"+str(tile[0])+"⌝"
@@ -9,20 +14,31 @@ def showTile(lines, tile):
     lines[-1] += "⌞"+str(tile[2])+"⌟"
 
 # organizuje wejście
-def getInput(N, tiles):
+def getInput(tiles):
 
     N = int(input())
     for i in range(N*N):
          tiles.append(input().split(" "))
+         for j in range(len(tiles[-1])): tiles[-1][j] = int(tiles[-1][j])
 
-def classifyTiles(tiles, corners, sides, middles):
-    corners = []
-    sides = []
-    middles = []
+# obrót kafelka r-razy o 90 stopni, zgodnie ze wskazówkami zegara
+def rotateTile(t, r):
+    return [t[-r], t[1-r], t[2-r], t[3-r]]
+
+# klasyfikowanie i organizacja kafelków
+def classifyTiles(tiles):
     for t in tiles:
-        temp = sum(tiles[i] == 0 for i in range(4))
-        if   temp == 2: corners.append(t)
-        elif temp == 1: sides.append(t)
+        temp = sum((t[i] == 0) for i in range(4))
+        if   temp == 2: # obracanie w taki sposób, żeby wartości 0 znajdowały się przy prawych górnym rogu
+            if   t[2] != 0 and t[3] == 0: corners.append(rotateTile(t, 0))
+            elif t[3] != 0 and t[0] == 0: corners.append(rotateTile(t, 3))
+            elif t[0] != 0 and t[1] == 0: corners.append(rotateTile(t, 2))
+            else                        : corners.append(rotateTile(t, 1))
+        elif temp == 1: # obracanie w taki sposób, żeby wartości 0 znajdowały się przy górnej krawędzi
+            if   t[0] == 0: sides.append(rotateTile(t, 0))
+            elif t[1] == 0: sides.append(rotateTile(t, 3))
+            elif t[2] == 0: sides.append(rotateTile(t, 2))
+            else          : sides.append(rotateTile(t, 1))
         elif temp == 0: middles.append(t)
         else: print("!BŁĄD! Kafelek z błędną liczbą krawędzi bocznych")
 
@@ -30,123 +46,75 @@ def classifyTiles(tiles, corners, sides, middles):
 def getRotations(ogMiddles):
     middles = []
     for t in ogMiddles:
-        middles.append(t)
-        middles.append([t[3], t[0], t[1], t[2]])
-        middles.append([t[2], t[3], t[0], t[1]])
-        middles.append([t[1], t[2], t[3], t[0]])
+        for r in range(4):
+            middles.append(rotateTile(t, r))
     return middles
 
+# zwraca sumę zmiennej obecności wszystkich kafelków na danych koordynatach
+def getBoolAtCoords(grid, x, y):
+    if   (x == 0 or x == N-1) and (y == 0 or y == N-1): # Rogi
+        return sum(grid[x, y, n] for n in range(len(corners)))
+    elif (x == 0 or x == N-1) or  (y == 0 or y == N-1): # Brzegi
+        return sum(grid[x, y, n] for n in range(len(sides)))
+    else:
+        return sum(grid[x, y, n] for n in range(len(middles)))
+
+# zwraca sumę krawędzi wszystkich kafelków na danych koordynatach i na danej stronie
+def getValueAtCoords(grid, x, y, s):
+    if   (x == 0 or x == N-1) and (y == 0 or y == N-1): # Rogi
+        if   x == N-1 and y == 0  : s -= 1
+        elif x == N-1 and y == N-1: s -= 2
+        elif x == 0   and y == N-1: s -= 3
+        s = s % 4
+        return sum(grid[x, y, n] * corners[n][s] for n in range(len(corners)))
+    elif (x == 0 or x == N-1) or  (y == 0 or y == N-1): # Brzegi
+        if x == N-1: s -= 1
+        if y == N-1: s -= 2
+        if x == 0  : s -= 3
+        s = s % 4
+        return sum(grid[x, y, n] * sides[n][s] for n in range(len(sides)))
+    else:
+        return sum(grid[x, y, n] * middles[n][s] for n in range(len(middles)))
+
 # kluczowa funkcja, rozwiązuje problem optymalizacją liniową
-def solve(N, tiles):
+def solve():
     lp = MixedIntegerLinearProgram(maximization=True)
     grid = lp.new_variable(binary=True)
     lp.set_objective(1)
 
-    # warunki na rogi
-    lp.add_constraint(sum(grid[0  , 0  , n] for n in range(len(corners))) == 1)
-    lp.add_constraint(sum(grid[N-1, 0  , n] for n in range(len(corners))) == 1)
-    lp.add_constraint(sum(grid[N-1, N-1, n] for n in range(len(corners))) == 1)
-    lp.add_constraint(sum(grid[0  , N-1, n] for n in range(len(corners))) == 1)
+    # warunek jednokrotnego wykorzystania kafelka
+    for x in range(N):
+        for y in range(N):
+            lp.add_constraint(getBoolAtCoords(grid, x, y) == 1)
 
-    # warunki na brzegi
-    # górny brzeg
-    for x in range(1, N-2): lp.add_constraint(sum(grid[x  , 0  , n] for n in range(len(sides))) == 1)
-    # prawy brzeg
-    for y in range(1, N-2): lp.add_constraint(sum(grid[N-1, y  , n] for n in range(len(sides))) == 1)
-    # dolny brzeg
-    for x in range(1, N-2): lp.add_constraint(sum(grid[x  , N-1, n] for n in range(len(sides))) == 1)
-    # lewy brzeg
-    for y in range(1, N-2): lp.add_constraint(sum(grid[0  , y  , n] for n in range(len(sides))) == 1)
-
-    # warunki na środki
-    # ograniczenia na przypisanie jednego i tylko jednego środka do danej kratki
-    for x in range(1, N-1):
-        for y in range(1, N-1):
-            lp.add_constraint(sum(grid[x, y, n] for n in range(len(middles))) == 1)
-
-    # ograniczenia na przypisanie jednego i tylko jednego obrotu środka do jakiejkolwiek kratki
+    # warunek jednokrotnego wykorzystania obrotu kafelka środkowego
     for n in range(0, len(middles), 4):
         lp.add_constraint(sum(sum(sum(grid[x, y, n+i] for i in range(4))
                                   for x in range(1, N-1))
                               for y in range(1, N-1)) == 1)
 
-    # krańce planszy
-    # górna ściana
-    lp.add_constraint(sum(grid[0, 0, n] * corners[n][0] for n in range(len(corners))) == 0)
-    for x in range(1, N-1):
-        lp.add_constraint(sum(grid[N-1, 0, n] * sides[n][0] for n in range(len(sides))) == 0)
+    # warunek krawędzi z wartością 0 na zewnątrz
+    # górny brzeg
+    for x in range(N):
+        lp.add_constraint(getValueAtCoords(grid, x  , 0  , 0) == 0)
+    # prawy brzeg
+    for y in range(N):
+        lp.add_constraint(getValueAtCoords(grid, N-1, y  , 1) == 0)
+    # dolny brzeg
+    for y in range(N):
+        lp.add_constraint(getValueAtCoords(grid, x  , N-1, 2) == 0)
+    # prawy brzeg
+    for y in range(N):
+        lp.add_constraint(getValueAtCoords(grid, 0  , y  , 3) == 0)
+
+    # kluczowy warunek: wszystkie krawędzie muszą mieć takie same wartości na obu kafelkach
+    for x in range(N):
+        for y in range(N):
+            if x < N-1: lp.add_constraint(getValueAtCoords(grid, x  , y  , 1) ==
+                                          getValueAtCoords(grid, x+1, y  , 3))
+            if y < N-1: lp.add_constraint(getValueAtCoords(grid, x  , y  , 2) ==
+                                          getValueAtCoords(grid, x  , y+1, 0))
     
-
-    # połączenia róg - brzeg
-    # róg 0, 0
-    lp.add_constraint(sum(grid[0, 0, n] * corners[n][1] for n in range(len(corners))) ==
-                      sum(grid[1, 0, n] * sides  [n][3] for n in range(len(sides  ))))    
-    lp.add_constraint(sum(grid[0, 0, n] * corners[n][2] for n in range(len(corners))) ==
-                      sum(grid[0, 1, n] * sides  [n][0] for n in range(len(sides  ))))
-    # róg N-1, 0
-    lp.add_constraint(sum(grid[N-1, 0, n] * corners[n][3] for n in range(len(corners))) ==
-                      sum(grid[N-2, 0, n] * sides  [n][1] for n in range(len(sides  ))))    
-    lp.add_constraint(sum(grid[N-1, 0, n] * corners[n][2] for n in range(len(corners))) ==
-                      sum(grid[N-1, 1, n] * sides  [n][0] for n in range(len(sides  ))))
-    # róg N-1, N-1
-    lp.add_constraint(sum(grid[N-1, N-1, n] * corners[n][3] for n in range(len(corners))) ==
-                      sum(grid[N-2, N-1, n] * sides  [n][1] for n in range(len(sides  ))))    
-    lp.add_constraint(sum(grid[N-1, N-1, n] * corners[n][0] for n in range(len(corners))) ==
-                      sum(grid[N-1, N-2, n] * sides  [n][2] for n in range(len(sides  ))))
-    # róg 0, N-1
-    lp.add_constraint(sum(grid[0, N-1, n] * corners[n][1] for n in range(len(corners))) ==
-                      sum(grid[1, N-1, n] * sides  [n][3] for n in range(len(sides  ))))    
-    lp.add_constraint(sum(grid[0, N-1, n] * corners[n][0] for n in range(len(corners))) ==
-                      sum(grid[0, N-2, n] * sides  [n][2] for n in range(len(sides  ))))
-
-    # połączenia brzeg-brzeg
-    # górna ściana
-    for x in range(1, N-2): # bez rogów i ostatniego na ściance (bo ten łączy się z rogiem)
-        lp.add_constraint(sum(grid[x, 0, n] * sides[n][1] - grid[x+1, 0, n] * sides[n][3]
-                              for n in range(len(sides))) == 0)
-    # prawa ściana
-    for y in range(1, N-2): # bez rogów i ostatniego na ściance (bo ten łączy się z rogiem)
-        lp.add_constraint(sum(grid[N-1, y, n] * sides[n][2] - grid[N-1, y+1, n] * sides[n][3]
-                              for n in range(len(sides))) == 0)
-    # dolna ściana
-    for x in range(1, N-2): # bez rogów i ostatniego na ściance (bo ten łączy się z rogiem)
-        lp.add_constraint(sum(grid[x, N-1, n] * sides[n][1] - grid[x+1, N-1, n] * sides[n][3]
-                              for n in range(len(sides))) == 0)
-    # lewa ściana
-    for y in range(1, N-2): # bez rogów i ostatniego na ściance (bo ten łączy się z rogiem)
-        lp.add_constraint(sum(grid[0, y, n] * sides[n][2] - grid[0, y+1, n] * sides[n][3]
-                              for n in range(len(sides))) == 0)
-
-    # połączenia brzeg-środek
-    # górna ściana
-    for x in range(1, N-1): # bez rogów i ostatniego na ściance (bo ten łączy się z rogiem)
-        lp.add_constraint(sum(grid[x, 0, n] * sides  [n][2] for n in range(len(sides  ))) ==
-                          sum(grid[x, 0, n] * middles[n][0] for n in range(len(middles))))
-    # prawa ściana
-    for y in range(1, N-1): # bez rogów i ostatniego na ściance (bo ten łączy się z rogiem)
-        lp.add_constraint(sum(grid[N-1, y, n] * sides  [n][3] for n in range(len(sides  ))) ==
-                          sum(grid[N-2, y, n] * middles[n][1] for n in range(len(middles))))
-    # dolna ściana
-    for x in range(1, N-1): # bez rogów i ostatniego na ściance (bo ten łączy się z rogiem)
-        lp.add_constraint(sum(grid[x, N-1, n] * sides  [n][0] for n in range(len(sides  ))) ==
-                          sum(grid[x, N-1, n] * middles[n][2] for n in range(len(middles))))
-    # lewa ściana
-    for y in range(1, N-1): # bez rogów i ostatniego na ściance (bo ten łączy się z rogiem)
-        lp.add_constraint(sum(grid[0, y, n] * sides  [n][1] for n in range(len(sides  ))) ==
-                          sum(grid[1, y, n] * middles[n][3] for n in range(len(middles))))
-
-    # połączenia środek-środek
-
-
-    # kluczowy warunek na krawędzie: kafelki po obu stronach musza mieć tę samą wartość na odpowiadających bokach
-    for x in range(1, N-1):
-        for y in range(1, N-1):
-            if x < N-2: lp.add_constraint(sum(grid[x  , y, n] * middles[n][1] - grid[x+1, y, n] * middles[n][3]
-                                              for n in range(len(middles))) == 0)
-            
-            if y < N-2: lp.add_constraint(sum(grid[x, y  , n] * middles[n][2] - grid[x, y+1, n] * middles[n][0]
-                                              for n in range(len(middles))) == 0)
-
     lp.solve()
     dict = lp.get_values(grid)
     result = [[-1 for x in range(N)] for y in range(N)] # pairs of [n, s], where n is index of tile and s is rotation
@@ -160,25 +128,31 @@ def solve(N, tiles):
 
 
 
-N = 4 # NxN grid
+
 ogTiles = []
 
-getInput(N, ogTiles)
-corners = []
-sides = []
-middles = []
-classifyTiles(ogTiles, corners, sides, middles)
+getInput(ogTiles)
+classifyTiles(ogTiles)
 middles = getRotations(middles)
          
-result = solve(N, corners, sides, middles)
+result = solve()
 
 representation = []
-for r in range(len(result)):
+for x in range(len(result)):
     for i in range(3): representation.append("")
-    for c in range(len(result[r])):
-        if   r == c and c == 0: showTile(representation, corners[result[r][c]])
-        elif r == 0 or  c == 0: showTile(representation, sides  [result[r][c]])
-        else                  : showTile(representation, middles[result[r][c]])
+    for y in range(len(result[x])):
+        s = 0
+        if   (x == 0 or x == N-1) and (y == 0 or y == N-1):            
+            if   x == N-1 and y == 0  : s = 3
+            elif x == N-1 and y == N-1: s = 2
+            elif x == 0   and y == N-1: s = 1
+            showTile(representation, rotateTile(corners[result[x][y]], s))
+        elif (x == 0 or x == N-1) or  (y == 0 or y == N-1): # Brzegi
+            if   y == N-1: s = 1
+            elif x == N-1: s = 2
+            elif y == 0  : s = 3
+            showTile(representation, rotateTile(sides  [result[x][y]], s))
+        else: showTile(representation, middles[result[x][y]])
 
 for r in representation:
     print(r)
